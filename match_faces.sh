086ce8e18fcd6db2f63f7fd29b379eb7f77d79df
Umair Fayaz <59157924+umair-fayaz@users.noreply.github.com>
2020-05-17 13:11:54 +0530

    Update match_faces.sh

--- match_faces.sh
+++ match_faces.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Test face matches using pickle file to classify matching images
-python ~/facial_recognition_based_attendance_system/src/classifier.py CLASSIFY \
-~/facial_recognition_based_attendance_system/data/images/test_aligned/ \
-~/facial_recognition_based_attendance_system/models/20180402-114759.pb \
-~/facial_recognition_based_attendance_system/models/my_classifier.pkl
+python ~/facial-recognition-based-attendance-system/src/classifier.py CLASSIFY \
+~/facial-recognition-based-attendance-system/data/images/test_aligned/ \
+~/facial-recognition-based-attendance-system/models/20180402-114759.pb \
+~/facial-recognition-based-attendance-system/models/my_classifier.pkl
